76c49734f96847d14f9431d9949393960bcea8ba
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2017-05-14 23:42:05 +0200

    Corrección de comandos internos que pueden ser forked. - Antes, no se podía ejecutar un comando interno si no era el primero en la tubería.

--- src/shell.c
+++ src/shell.c
@@ -279,8 +279,9 @@ void put_job_background(Job * job) {
     print_info("Background job ... pid : %d, command : %s\n", job->gpid, job->command);
 }
 
-void launch_process(Process * p, int fdin, int fdout, pid_t gpid, char foreground, int icmd) {
+void launch_process(Process * p, int fdin, int fdout, pid_t gpid, char foreground) {
     pid_t pid;
+    int icmd;
     
     pid = getpid();
     
@@ -295,6 +296,7 @@ void launch_process(Process * p, int fdin, int fdout, pid_t gpid, char foregroun
     signal(SIGCHLD, SIG_DFL);
     control_signals(SIG_DFL);
 
+    icmd = indexOfInternalProcess(p);
     // Si no es un comando interno, o este no tiene manejador
     if (icmd < 0 || !ICMD_HANDLER(icmd)) { 
         execvp(p->args[0], p->args);
@@ -309,14 +311,14 @@ void launch_process(Process * p, int fdin, int fdout, pid_t gpid, char foregroun
     
 }
 
-void launch_forked_job(Job * job, int icmd) {
+void launch_forked_job(Job * job) {
     Process * p = job->proc;
     
     while (p) {
         p->pid = fork(); 
         
         if (p->pid == 0) 
-            launch_process(p, shell.fdin, STDOUT_FILENO,job->gpid,job->foreground, icmd);
+            launch_process(p, shell.fdin, STDOUT_FILENO,job->gpid,job->foreground);
         else {
             
             if (job->gpid == 0)
@@ -344,13 +346,13 @@ void launch_forked_job(Job * job, int icmd) {
  * @return  índice del trabajo interno, o -1 en caso de que no sea interno.
  */
 
-int indexInternalJob(Job * job) {
+int indexOfInternalProcess(Process * p) {
     int index = -1;
     int j = 0;
     
     while (index == -1 && j < ICMD_TOTAL) {
         
-        if (strcmp(job->proc->args[0], ICMD_STR(j)) == 0)
+        if (strcmp(p->args[0], ICMD_STR(j)) == 0)
             index = j;
         
         j++;
@@ -366,7 +368,7 @@ void launch_job(Job * job) {
     if (job == NULL) // Causado por una línea vacía por el 
         return;
     
-    index = indexInternalJob(job);
+    index = indexOfInternalProcess(job->proc);
     
     if (index >= 0 && ICMD_HANDLER(index) && !ICMD_FORK(index)) {
         job->gpid = -1;
@@ -376,7 +378,7 @@ void launch_job(Job * job) {
         unblock_sigchld();
     }
     else
-        launch_forked_job(job, index);
+        launch_forked_job(job);
     
 }
 
@@ -415,7 +417,7 @@ void cmd_rr_handler(Process * p) {
         p->pid = fork();
         
         if (!p->pid)
-            launch_process(p,shell.fdin,STDOUT_FILENO,job->gpid,job->foreground,-1);
+            launch_process(p,shell.fdin,STDOUT_FILENO,job->gpid,job->foreground);
         // padre.
         
         if (job->gpid == -1)
